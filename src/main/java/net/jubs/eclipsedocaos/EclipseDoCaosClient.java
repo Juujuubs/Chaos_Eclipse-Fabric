74da99e5a07231828218fc25acf4eb347a62dc35
Ju <gabrielgcm.martins@gmail.com>
2024-02-27 03:26:23 -0300

    Remoção do Hello World

---
+++ src/main/java/net/jubs/eclipsedocaos/EclipseDoCaosClient.java
@@ -0,0 +1,11 @@
+package net.jubs.eclipsedocaos;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EclipseDoCaosClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/jubs/eclipsedocaos/EclipseDoCaosDataGenerator.java
@@ -0,0 +1,11 @@
+package net.jubs.eclipsedocaos;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class EclipseDoCaosDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
